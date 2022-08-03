26c915f796ed9cfd5eafad0ea6815f736c3ecc01
jackdixonryan <jack.dixon.ryan@gmail.com>
2022-08-03 11:03:13 -0600

    Works well enough to accept inputs, so that's pretty cool

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use std::io;
 
 fn main() {
     // two executable functions: get_code and enter_code. 
-    let the_code: i32 = generate_code();
     println!("type get_code or enter_code");
     println!("type s to stop.");
-    let mut input = String::new();
+
+    let the_code: i32 = generate_code();
+    let mut open = false;
 
     loop { 
+        let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
             .expect("failed to read input.");
@@ -17,11 +19,23 @@ fn main() {
         if trimmed_input == "s" {
             println!("terminating_program...");
             break;
+        } else if trimmed_input == "get_code" {
+            println!("CODE: {}", the_code);
+        } else if trimmed_input == "enter_code" {
+            println!("enter code:");
+            let code_is_correct = handle_code_entry(the_code);
+            if code_is_correct {
+                open = true;
+                println!("keycode_accepted. unlocking...");
+            } else {
+                println!("invalid keycode. try again.");
+            }
+        } else if trimmed_input == "check_is_unlocked" {
+            println!("UNLOCKED: {}", open);
+        } else {
+            println!("invalid_input");
         }
     }
-
-    
-
 }
 
 // in theory, confers ownership.
@@ -31,5 +45,16 @@ fn generate_code() -> i32 {
     a_code
 }
 
-
-
+fn handle_code_entry(the_code: i32) -> bool {
+    let mut code_guess = String::new();
+    io::stdin() 
+        .read_line(&mut code_guess)
+        .expect("failed to read input.");
+    
+    let trimmed_code_guess: i32 = code_guess.trim().parse().expect("must be a number.");
+    if the_code == trimmed_code_guess {
+        true 
+    } else {
+        false
+    }
+}
