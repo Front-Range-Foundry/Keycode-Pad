0317c5cc6267b2355aace09765a690b2a1a02fdb
jackdixonryan <jack.dixon.ryan@gmail.com>
2022-08-03 14:41:50 -0600

    Final.
    - added a multiselect
    - capped entry of code at 3 guesses.
    - better logging stuff.
    - added some confirms.

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,71 @@
 use rand::Rng;
 use std::io;
-use dialoguer::Confirm;
+use dialoguer::{
+    Select,
+    Confirm,
+    theme::ColorfulTheme
+};
 
-fn main() {
-    println!("KEYPAD_OPTIONS:");
-    println!("- get_code");
-    println!("- enter_code");
-    println!("- check_is_unlocked");
-    println!("- lock");
-    println!("- end");
+fn main() -> std::io::Result<()> {
 
+    println!("BEGIN KEYCODE PROGRAM ------------------------------");
     let mut the_code: i32 = generate_code();
     let mut open = false;
+    let mut remaining_attempts = 3;
+    'main_program: loop { 
+        let items = vec![
+            "Get Code",
+            "Enter Code",
+            "Check Lock Status",
+            "Lock",
+            "End Program"
+        ];
+    
+        let chosen : usize = Select::with_theme(&ColorfulTheme::default())
+            .items(&items)
+            .default(0)
+            .interact()?;
 
-    loop { 
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("failed to read input.");
-        
-        let sanitized_input = input.trim();
-        match sanitized_input {
-            "end" => {
-                println!("terminating_program...");
-                break;
+
+        match chosen {
+            4 => {
+                break 'main_program;
             },
-            "get_code" => {
+            0 => {
                 println!("CODE: {}", the_code);
             },
-            "enter_code" => {
-                println!("enter code:");
-                let code_is_correct = handle_code_entry(the_code);
-                if code_is_correct {
-                    open = true;
-                    println!("keycode_accepted. unlocking...");
-                } else {
-                    println!("invalid keycode. try again.");
+            1 => {
+                'code_entry: loop { 
+                    println!("enter code:");
+                    let code_is_correct = handle_code_entry(the_code);
+                    if code_is_correct {
+                        if Confirm::new().with_prompt("Keycode accepted. Unlock?").interact()? {
+                            open = true;
+                            println!("keycode accepted. unlocked.");
+                            break 'code_entry;
+                        } else {
+                            println!("not unlocking.");
+                            break 'code_entry;
+                        }
+                    } else {
+                        if remaining_attempts > 1 {
+                            remaining_attempts = remaining_attempts - 1;
+                            println!("invalid keycode. {} attempts remaining.", remaining_attempts);
+                        } else {
+                            println!("too many attempts. module inaccessible.");
+                            break 'code_entry;
+                        }
+                    }
                 }
             },
-            "check_is_unlocked" => {
-                println!("UNLOCKED: {}", open);
+            2 => {
+                println!("LOCKED: {}", !open);
             },
-            "lock" => {
+            3 => {
                 if open {
-                    if Confirm::new().with_prompt("confirm").interact()? {
-                        open = false;
-                        the_code = generate_code();
-                    } else {
-                        println!("nah");
-                    }
+                    open = false;
+                    the_code = generate_code();
+                    println!("successful lock.")
                 } else {
                     println!("already_locked");
                 }
@@ -58,6 +75,8 @@ fn main() {
             }
         }
     }
+    println!("END KEYCODE PROGRAM ------------------------------");
+    Ok(()) // to satisfy returning a Result
 }
 
 // in theory, confers ownership.
