2e046f8923520a52a53df427f44f7c82ac16a279
jackdixonryan <jack.dixon.ryan@gmail.com>
2022-08-03 11:13:47 -0600

    Replaced if statement with match statement. Looks a little cleaner to me. I think it should probably have slightly better control flow on some of the functions. It should also have an options function. Possibly do some fun stuff in the terminal.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ use rand::Rng;
 use std::io;
 
 fn main() {
-    // two executable functions: get_code and enter_code. 
-    println!("type get_code or enter_code");
-    println!("type s to stop.");
+    println!("KEYPAD_OPTIONS:");
+    println!("- get_code");
+    println!("- enter_code");
+    println!("- check_is_unlocked");
+    println!("- lock");
+    println!("- end");
 
-    let the_code: i32 = generate_code();
+    let mut the_code: i32 = generate_code();
     let mut open = false;
 
     loop { 
@@ -15,25 +18,39 @@ fn main() {
             .read_line(&mut input)
             .expect("failed to read input.");
         
-        let trimmed_input = input.trim();
-        if trimmed_input == "s" {
-            println!("terminating_program...");
-            break;
-        } else if trimmed_input == "get_code" {
-            println!("CODE: {}", the_code);
-        } else if trimmed_input == "enter_code" {
-            println!("enter code:");
-            let code_is_correct = handle_code_entry(the_code);
-            if code_is_correct {
-                open = true;
-                println!("keycode_accepted. unlocking...");
-            } else {
-                println!("invalid keycode. try again.");
+        let sanitized_input = input.trim();
+        match sanitized_input {
+            "end" => {
+                println!("terminating_program...");
+                break;
+            },
+            "get_code" => {
+                println!("CODE: {}", the_code);
+            },
+            "enter_code" => {
+                println!("enter code:");
+                let code_is_correct = handle_code_entry(the_code);
+                if code_is_correct {
+                    open = true;
+                    println!("keycode_accepted. unlocking...");
+                } else {
+                    println!("invalid keycode. try again.");
+                }
+            },
+            "check_is_unlocked" => {
+                println!("UNLOCKED: {}", open);
+            },
+            "lock" => {
+                if open {
+                    open = false;
+                    the_code = generate_code();
+                } else {
+                    println!("already_locked");
+                }
+            },
+            _ => {
+                println!("invalid_input");
             }
-        } else if trimmed_input == "check_is_unlocked" {
-            println!("UNLOCKED: {}", open);
-        } else {
-            println!("invalid_input");
         }
     }
 }
